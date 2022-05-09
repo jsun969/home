d6f313273b5836490ec04296cafaf27b8dc5ac3d
Justin Sun <i@jsun969.cn>
2022-05-09 16:47:52 +0800

    feat: add avatar and descriptions

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div className="flex h-screen">
-      <div className="m-auto">
-        <div className="text-5xl">您好, 我是荆棘Justin</div>
+      <div className="m-auto text-center flex flex-col gap-6">
+        <img
+          alt="avatar"
+          src="/avatar.png"
+          className="m-auto border-2 w-32 rounded-full"
+        />
+        <div className="text-5xl">您好, 我是荆棘</div>
+        <div>
+          男 / {new Date().getFullYear() - 2005}岁 / 上海人 / Typescript全栈 /
+          React & Nest
+        </div>
       </div>
     </div>
   );
