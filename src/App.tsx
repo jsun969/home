de5ff920cdcb9e631d3f46a754facda1dc44b691
Justin Sun <i@jsun969.cn>
2022-05-12 16:35:02 +0800

    feat: add i18n title and description

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import AvatarTitleDesc from './components/AvatarTitleDesc';
 import LanguageToggleButton from './components/LanguageToggleButton';
 import SocialInfo from './components/SocialInfo';
 import ThemeToggleButton from './components/ThemeToggleButton';
 
 const App: React.FC = () => {
+  const { t } = useTranslation();
+  useEffect(() => {
+    document.title = t('title');
+  }, [t]);
+
   return (
     <div className="flex h-screen dark:bg-slate-900 relative">
       <div className="absolute top-4 right-4">
