a0c674761da477c813fbfe30e49966486cb40fef
Justin Sun <i@jsun969.cn>
2022-06-24 07:15:42 +0800

    feat: update full stack to frontend

--- src/components/AvatarTitleDesc.tsx
+++ src/components/AvatarTitleDesc.tsx
@@ -14,8 +14,8 @@ const AvatarTitleDesc: React.FC = () => {
       <div className="text-3xl sm:text-5xl dark:text-white">{t('greet')}</div>
       <div className="text-sm sm:text-base text-slate-700 dark:text-slate-400">
         {t('description.male')} / {new Date().getFullYear() - 2005}
-        {t('description.age')} / {t('description.shanghai')} / Typescript{' '}
-        {t('description.full-stack')} / React & Nest
+        {t('description.age')} / {t('description.shanghai')} /{' '}
+        {t('description.frontend')} / Typescript {t('description.lover')}
       </div>
     </>
   );
